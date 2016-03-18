f73f09a237e0b83ba20683a9e8aad6e6e2660a99
cghil <cghilarducci@gmail.com>
2016-03-18 16:02:59 -0700

    added get, post routes for /posts

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var app = express();
+var dbUri = "mongodb://localhost:27017/api";
+
+var dbConnection = mongoose.createConnection(dbUri);
+
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema ({
+	title: {
+		type: String,
+		required: true,
+		trim: true	
+	},
+	text: {
+		type: String,
+		required: true
+	}
+});
+
+var Post = dbConnection.model('Post', postSchema, 'posts');
+// name, schema, name of collection
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/', function(req, res){
+	res.send('ok');
+});
+
+app.get('/posts', function(req, res){
+	Post.find({}, function(error, posts){
+		if (error) return next(error);
+		res.send(posts);	
+	});
+});
+
+app.post('/posts', function(req, res, next){
+	var post = new Post(req.body)
+	post.save(function(error, results){
+		if (error) return next(error);
+		res.send(results);
+	});
+
+});
+var server = require('http').createServer(app).listen(3000);
+
+// mongoose.connect('mongodb://localhost/test');
+

03d18016cb7ed770827084bdceeba38ae2915755
cghil <cghilarducci@gmail.com>
2016-03-23 16:44:46 -0700

    schema validations

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var errorHandler = require('errorhandler')
+var errorhandler = require('errorhandler')
 
 var app = express();
 var dbUri = "mongodb://localhost:27017/api";
@@ -10,16 +10,29 @@ var dbUri = "mongodb://localhost:27017/api";
 var dbConnection = mongoose.createConnection(dbUri);
 
 var Schema = mongoose.Schema;
-
 var postSchema = new Schema ({
 	title: {
 		type: String,
 		required: true,
-		trim: true	
+		trim: true,
+		match: /^([\w ,.!?]{1,100})$/
 	},
 	text: {
 		type: String,
-		requireda: true
+		required: true,
+		max: 2000
+	},
+	viewCounter: Number,
+	published: Boolean,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		required: true
+	},
+	updateAt: {
+		type: Date,
+		default: Date.now,
+		required: true
 	}
 });
 
@@ -28,27 +41,32 @@ var Post = dbConnection.model('Post', postSchema, 'posts');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function(req, res){
 	res.send('ok');
 });
 
-app.get('/posts', function(req, res){
-	Post.find({}, function(error, posts){
-		if (error) return next(error);
-		res.send(posts);	
-	});
+app.get('/posts', function(req, res, next){
+  Post.find({}, function(error, posts){
+    if (error) res.send(error);
+    res.send(posts);
+  });
 });
 
 app.post('/posts', function(req, res, next){
 	var post = new Post(req.body)
-	post.save(function(error, results){
-		if (error) return next(error);
-		res.send(results);
+	post.validate(function(error){
+		console.log(error);
+		if(error) return res.send(error)
+		post.save(function(error, results){
+			if (error) res.send(error);
+			res.send(results);
+		});
 	});
 
 });
 
-app.use(errorhandler);
+app.use(errorhandler());
 
 var server = require('http').createServer(app).listen(3000);
 
